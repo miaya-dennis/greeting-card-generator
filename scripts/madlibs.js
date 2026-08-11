322604a744aaf7602adf4b74aadd236cb8190f01
Miaya <myfirefly22@gmail.com>
2026-08-10 21:38:36 -0400

    name the card block

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -2,6 +2,12 @@ let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
 
-console.log("Dear " + name + ",");
-console.log("You are as " + adjective + " as a " + noun + ".");
-console.log("Happy birthday from the generator. ");
+function makeCard(){
+  console.log("Dear " + name + ",");
+  console.log("You are as " + adjective + " as a " + noun + ".");
+  console.log("Happy birthday from the generator. ");
+
+}
+
+  makeCard();
+  makeCard();
