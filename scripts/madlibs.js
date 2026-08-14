0f2478035ccc42b171e7edf30b16faedbbcc7e79
Miaya <myfirefly22@gmail.com>
2026-08-13 22:16:37 -0400

    random closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -2,6 +2,13 @@ let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
 
+let closings = [
+
+"You are a legend.",
+"Stay golden.",
+"Signs point to awesome.",
+];
+
 function makeCard(){
   console.log("Dear " + name + ",");
   console.log("You are as " + adjective + " as a " + noun + ".");
